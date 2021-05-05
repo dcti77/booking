479b48eb6e2bc756534ace829fe941ec2feae3c6
Vitali Kondrus <vkondrus@yahoo.com>
2021-05-05 19:27:36 +0300

    Some changes for Traves Ci OK
    .idea/ added in .gitignore

--- booking/users/views.py
+++ booking/users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+
+
+# Login
+def user_login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+    else:
+        print("Неверный логин либо пароль")
+
+
+# Logout
+def user_logout(request):
+    logout(request)
 
 # Create your views here.
