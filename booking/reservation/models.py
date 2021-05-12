cc612742d545a4e3a7d45a21cda3dde5f1e5a8fb
Vital Shved <Skandinav_by@mail.ru>
2021-05-12 19:33:47 +0300

    fix flake8

--- booking/reservation/models.py
+++ booking/reservation/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Reservation(models.Model):
     country = models.CharField(max_length=250)
     hotel = models.CharField(max_length=250)
@@ -9,3 +9,14 @@ class Reservation(models.Model):
     booking_date = models.DateField()
     rating = models.IntegerField()
 
+    def __str__(self):
+        return self.country, self.hotel
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=80)
+    middle_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    phone = models.CharField(max_length=100, unique=True)
+    card = models.DateField(max_digits=16, decimal_places=0)
+    valid_thru = models.DateField()
