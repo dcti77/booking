a8c474dfafad040e5c844fd640f945ca52e2d2fb
AladorDev <alador@yandex.ru>
2021-06-07 22:13:31 +0300

    Adding the ability to search by parameter

--- booking/hotels/urls.py
+++ booking/hotels/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import SearchResultsView
 
 
 urlpatterns = [
     path('', views.hotel, name='hotel'),
+    path('search/', SearchResultsView.as_view(), name='search_results'),
 ]
--- booking/hotels/views.py
+++ booking/hotels/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from hotels.models import Hotel
 from django.views.generic.list import ListView
 
+
 def hotel(request):
     if request.method == 'GET':
-        print (Hotel.objects.all())
+        print(Hotel.objects.all())
         return render(request, 'hotels/hotels.html', {'hotels': Hotel.objects.all()})
+
 
 class HotelView(ListView):
     model = Hotel
@@ -13,3 +15,7 @@ class HotelView(ListView):
     context_object_name = 'hotel'
 
 
+class SearchResultsView(ListView):
+    model = Hotel
+    template_name = 'hotels/search_results.html'
+    queryset = Hotel.objects.filter(country__icontains='United States')
